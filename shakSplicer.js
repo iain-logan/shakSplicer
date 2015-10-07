2e33ca4aae6d22917b3712393305bdf12c575ffc
Alex Jacobsen <kbb12160@uni.strath.ac.uk>
2015-10-07 15:18:26 +0100

    added path checking

--- shakSplicer.js
+++ shakSplicer.js
@@ -4,9 +4,21 @@ var parseString = require('xml2js').parseString;
 var sourcePath = process.argv[2],
     destinPath = process.argv[3];
 
-console.log('splicing xml files from ' + sourcePath + ' to ' + destinPath);
+var destPathStats = fs.statSync(destinPath);
 
-var files = fs.readdirSync(sourcePath);
+if(! destPathStats.isFile() && !destPathStats.isDirectory()){
+    fs.mkdirSync(destinPath);
+}else if(destPathStats.isFile()){
+console.log("Destination was a file");
+process.exit(1);
+}
+else{
+    console.log("Directory already exists");
+}
+
+
+console.log('splicing xml files from ' + sourcePath + ' to ' + destinPath);
+var files = fs.readirSync(sourcePath);
 
 console.log(files.filter( function (file) {
     return file.indexOf('.xml', file.length - 4) !== -1;
@@ -15,9 +27,14 @@ console.log(files.filter( function (file) {
 var parse = function (pathToFile) {
     fs.readFile(pathToFile, 'ascii', function (err, data) {
 	parseString(data, function (err, result) {
-	    console.log(result);
+	    splice(result, destinPath);
 	});
     });
 };
 
 parse(sourcePath + files[0]);
+
+var splice = function (tag, destination){
+   
+    
+}
