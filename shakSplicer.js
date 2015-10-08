da1406c63d6ec23db8a0faa5cc87fcd41e90e38c
iain-logan <iainlogan94@gmail.com>
2015-10-08 15:22:03 +0100

    Added help message and a few extra prints

--- shakSplicer.js
+++ shakSplicer.js
@@ -8,6 +8,15 @@ var sourcePath = process.argv[2],
     lock = false,
     builder = new xml2js.Builder({xmldec: { 'version': '1.0', 'encoding': 'ASCII', 'standalone': true}});
 
+if (process.argv[2] === "--help" || process.argv[2] === undefined || process.argv[2] === null) {
+    console.log("Usage: node shackSplicer.js sourcePath destinationPath\n");
+    console.log("This program will parse all xml files found at path sourcePath");
+    console.log("and then proceed to build up a directory represenation of the");
+    console.log("xml hierarchy at the output path, creating a file for each tag.");
+    process.exit(1);
+}
+
+
 var getNextNumber = function(callback){
     if(lock){
 	setTimeout(function () { getNextNumber(callback) },5);
@@ -22,29 +31,26 @@ var getNextNumber = function(callback){
 try{
     var destPathStats = fs.statSync(destinPath);
     if(destPathStats.isFile()){
-	console.log("Destination was a file");
+	console.log("Destination was a file\n");
 	process.exit(1);
     }
     else{
-	console.log("Directory already exists");
+	console.log("Directory already exists\n");
     }
 }catch(err){
     fs.mkdirSync(destinPath);
-    console.log("Directory doesn't exist so a new one has been made");
+    console.log("Directory doesn't exist so a new one has been made\n");
 }
 
 
 
-console.log('splicing xml files from ' + sourcePath + ' to ' + destinPath);
-var files = fs.readdirSync(sourcePath);
-
-console.log(files.filter( function (file) {
-    return file.indexOf('.xml', file.length - 4) !== -1;
-}));
+console.log('Splicing xml files from ' + sourcePath + ' to ' + destinPath + "...\n");
+var files = fs.readdirSync(sourcePath).filter( function (file) {
+        return file.indexOf('.xml', file.length - 4) !== -1;
+    });
 
 var parse = function (pathToFile) {
     fs.readFile(pathToFile, 'ascii', function (err, data) {
-	console.log(err);
 	parseString(data, function (err, result) {
 	    splice(result, destinPath);
 	});
@@ -89,6 +95,9 @@ var splice = function (tag, destination){
     });
 };
 
-files.forEach(function (elm) {
+console.log("Starting parsing and splicing of " + files.length + " file" + (files.length > 1 ? "s" : "") + "... this may take some time\n");
+
+files.forEach(function (elm, index) {
     parse(sourcePath + elm);
+    console.log("Started splicing " + elm + " [" + (index+1) + "/" + files.length + "]");
 });
